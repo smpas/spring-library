cfa182fd64f5799b5a3df1cf0ebde169bea50500
smpas <smpashts@gmail.com>
2024-02-26 15:35:11 +0300

    add advanced validation

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Neil Alishev
@@ -28,8 +29,16 @@ public class PersonDAO {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[] {email},
+                        new BeanPropertyRowMapper(Person.class))
+                .stream()
+                .findAny();
+    }
+
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[] {id},
+                        new BeanPropertyRowMapper<>(Person.class))
                 .stream()
                 .findAny()
                 .orElse(null);
